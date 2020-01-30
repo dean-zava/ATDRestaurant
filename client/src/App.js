da696de3a33da1903733f885d914c7a5d7f0dd37
Dean <deanza@post.bgu.ac.il>
2020-01-30 13:40:02 +0200

    toggler partially working

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppNavbar from './components/AppNavBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Majeek</h1>
+       <AppNavbar/> 
+    </div>
+  );
+}
+
+export default App;
