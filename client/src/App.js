35ff13f1657b1d1eb701e4e1d30c07203c820c17
Dean <deanza@post.bgu.ac.il>
2020-01-31 14:51:31 +0200

    items has been exported to redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import AppNavbar from './components/AppNavBar';
 import Restaurant from './components/Restaurant';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <h1> Kaki App for Majeed </h1>
       <AppNavbar />
       <Restaurant />
     </div>
+    </Provider>
   );
 }
 
