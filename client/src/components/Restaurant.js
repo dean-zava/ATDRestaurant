653a42d98629af6210dd2caf18c74dc8d48d1614
Dean <deanza@post.bgu.ac.il>
2020-02-03 00:09:21 +0200

    only logged users can view add reviews

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -6,6 +6,12 @@ import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class Restaurant extends Component {
+    static propTypes = {
+        getItems: PropTypes.func.isRequired,
+        item: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    }
+    
     componentDidMount() {
         this.props.getItems();
     }
@@ -24,13 +30,16 @@ class Restaurant extends Component {
                             {items.map(({ _id, name }) => (
                                 <CSSTransition key={_id} timeout={500} classNames="fade">
                                     <ListGroupItem>
+                                    { this.props.isAuthenticated ? (
                                     <Button
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
                                         onClick={this.onDeleteClick.bind(this, _id)}>
 
-                                        >&times;</Button>
+                                        >&times;</Button>)
+                                        :
+                                        null}
                                     {name}
                                     </ListGroupItem>
                                 </CSSTransition>
@@ -42,13 +51,9 @@ class Restaurant extends Component {
     }
 }
 
-Restaurant.propTypes = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
-}
-
-const mapStateToProps = (state) => ({
-    item: state.item
+const mapStateToProps = state => ({
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(
--- client/src/components/RestaurantModel.js
+++ client/src/components/RestaurantModel.js
@@ -11,11 +11,16 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ItemModal extends Component {
     state = {
         modal: false,
         name: ''
+    }
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool
     }
 
     toggle = () => {
@@ -42,12 +47,16 @@ class ItemModal extends Component {
     render() {
         return (
             <div>
+                {this.props.isAuthenticated ?
+
                 <Button
                 color="dark"
                 style={{marginBottom: '2rem'}}
                 onClick={this.toggle}
                 >Add Review</Button>
-
+                :
+                <h4 className="mb-3 ml-4">Please log in to manage items</h4>
+                }
                 <Modal
                 isOpen={this.state.modal}
                 toggle={this.toggle}
@@ -79,7 +88,8 @@ class ItemModal extends Component {
 }
 
 const mapStateToProps = state => ({
-    items: state.item 
+    items: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { addItem })(ItemModal);
