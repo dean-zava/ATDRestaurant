b2cf4e32fd057e69673db48ca517d5061edfd6bc
Dean <deanza@post.bgu.ac.il>
2020-02-03 01:00:06 +0200

    changing headline

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -61,7 +61,7 @@ class AppNavbar extends Component {
         <div>
             <Navbar color="dark" dark className="mb-5">
                 <Container>
-                    <NavbarBrand href="/">ShoppingList</NavbarBrand>
+                    <NavbarBrand href="/">Restaurant Reviews</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
