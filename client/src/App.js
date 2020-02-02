8eb600e3dc4e314f9746c112f8bdc3c1baa6ef27
Dean <deanza@post.bgu.ac.il>
2020-02-02 16:02:48 +0200

    authentication is handled in the reducer too

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavBar';
-import Restaurant from './components/Restaurant';
+import ShoppingList from './components/Restaurant';
 import ItemModal from './components/RestaurantModel';
 import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
 
-function App() {
-  return (
-    <Provider store={store}>
-    <div className="App">
-      <h1> Kaki App for Majeed </h1>
-      <AppNavbar />
-      <Container>
-        <ItemModal />
-        <Restaurant />
-      </Container>
-    </div>
-    </Provider>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className='App'>
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
